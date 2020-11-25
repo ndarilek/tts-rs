728c409e25341433f96ff6523f44a675a635058a
Nolan Darilek <nolan@thewordnerd.info>
2020-11-25 09:54:46 -0600

    Add example for unscientifically measuring latency of TTS.

---
+++ examples/latency.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+use tts::*;
+
+fn main() -> Result<(), Error> {
+    env_logger::init();
+    let mut tts = TTS::default()?;
+    println!("Press Enter and wait for speech.");
+    loop {
+        let mut _input = String::new();
+        io::stdin().read_line(&mut _input)?;
+        tts.speak("Hello, world.", true)?;
+    }
+}
