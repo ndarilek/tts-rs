d9ca83ca15ccf5ecd598e5995a6cfa8750bceda6
Nolan Darilek <nolan@thewordnerd.info>
2021-03-11 12:33:31 -0600

    WIP: Bump dependencies. Currently broken.

--- winrt_bindings/build.rs
+++ winrt_bindings/build.rs
@@ -1,7 +1,8 @@
 fn main() {
     windows::build!(
-        windows::media::core::MediaSource
-        windows::media::playback::{MediaPlaybackState, MediaPlayer}
-        windows::media::speech_synthesis::SpeechSynthesizer
+        windows::foundation::TypedEventHandler,
+        windows::media::core::MediaSource,
+        windows::media::playback::{MediaPlaybackState, MediaPlayer, MediaPlayerAudioCategory},
+        windows::media::speech_synthesis::SpeechSynthesizer,
     );
 }
